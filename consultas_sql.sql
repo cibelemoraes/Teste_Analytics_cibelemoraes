b549c4fc39d7e58cd63d68a8c9be8f0ea0a27ad7
Your Name <you@example.com>
2024-11-25 22:58:37 -0300

    grafico

--- consultas_sql.sql
+++ consultas_sql.sql
@@ -3,6 +3,12 @@
 
 SELECT Produto, Categoria,
        SUM(Quantidade * Preço) AS TOTAL_VENDAS
-FROM data_clean
+FROM data_clean.csv
 GROUP BY Produto, Categoria 
 ORDER BY TOTAL_VENDAS DESC;
+
+-- Para identificar os produtos que venderam menos no mês de junho de 2023
+
+SELECT Produto, Categoria,
+       SUM(Quantidade * Preço) AS TOTAL_VENDAS
+FROM data_clean.csv
